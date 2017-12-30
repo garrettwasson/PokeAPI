9ef1057e63e06a79eeb047079846b5e9c796530e
Garrett <garrettgucci@gmail.com>
2017-12-29 23:37:48 -0800

    initial commit

---
+++ PokeAPI.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	for(var i = 1; i < 152; i++){
+		$('body').append(`<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/` + i + `.png">`)
+	}
+	
+});
